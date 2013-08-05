604ab1f662b489349cc66cce83bdec79e2340211
SamM <sammulqueen.nz@gmail.com>
2013-08-05 20:00:09 +1200

    Updated server to use express

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-var app = require("http").createServer(server_handler),
-	io = require("socket.io").listen(app),
-	fs = require("fs");
+var express = require('express')
+  , app = express(),
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , fs = require("fs");
 
-function server_handler(req, res){
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
