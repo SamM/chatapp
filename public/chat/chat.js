9eb3b5216632a8b7198f0ca65083f0f27fcb12be
Sam Mulqueen <sammulqueen.nz@gmail.com>
2013-08-25 18:52:07 +1200

    Operator now displays typing notice. Next up will be for the chatter to do the same. Then notifying of read messages.

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -1,187 +1,194 @@
 var auth = {
-      "secret": "456",
-      "token": "123"
-  },
-  socket = null,
-  receive = {},
-  send = {},
-  chat = {};
+    "secret": "456",
+    "token": "123"
+},
+socket = null,
+receive = {},
+send = {},
+chat = {};
 
-	chat.connected = false;
-	chat.reconnecting = false;
-	chat.typing_timeout = null;
-	chat.unread_messages_here = false;
-	chat.unread_messages_there = false;
-	chat.operator_name = null;
+chat.connected = false;
+chat.reconnecting = false;
+chat.typing_timeout = null;
+chat.unread_messages_here = false;
+chat.unread_messages_there = false;
+chat.operator_name = null;
 
-  chat.add_notice = function(text, className){
+chat.add_notice = function(text, className) {
     className = className || "";
-    var notice = $('<div class="notice '+className+'">'+text+'</div>');
+    var notice = $('<div class="notice ' + className + '">' + text + '</div>');
     $("#messages").append(notice);
     chat.scroll_messages();
     return notice;
-  }
-  chat.add_message = function(username, text, className){
+}
+chat.add_message = function(username, text, className) {
     className = className || "";
-    var message = $('<div class="message '+className+'"><span class="username">'+username+':</span>'+text+'</div>');
+    var message = $('<div class="message ' + className + '"><span class="username">' + username + ':</span>' + text + '</div>');
     $("#messages").append(message);
     chat.scroll_messages();
     return message;
-  }
+}
 
-  chat.start_typing = function(){
-    if(chat.typing_timeout){
-      clearTimeout(chat.typing_timeout);
-		chat.typing_timeout = null;
-    }else{
-      send.start_typing();
+chat.start_typing = function() {
+    if (chat.typing_timeout) {
+        clearTimeout(chat.typing_timeout);
+        chat.typing_timeout = null;
+    } else {
+        send.start_typing();
     }
     chat.typing_timeout = setTimeout(chat.stop_typing, 1000);
     $("#message_input").css("border-color", "red");
-  }
+}
 
-  chat.stop_typing = function(){
+chat.stop_typing = function() {
     clearTimeout(chat.typing_timeout);
-	chat.typing_timeout = null;
+    chat.typing_timeout = null;
     send.stop_typing();
     $("#message_input").css("border-color", "#777777");
-  }
+}
 
-  chat.user_activity = function(){
-    if(chat.unread_messages_here){
-      send.read_message();
-      chat.unread_messages_here = false;
+chat.user_activity = function() {
+    if (chat.unread_messages_here) {
+        send.read_message();
+        chat.unread_messages_here = false;
     }
     chat.scroll_messages();
-  }
+}
 
-  chat.scroll_messages = function(){
+chat.scroll_messages = function() {
     $("#scroll").scrollTop($("#scroll")[0].scrollHeight);
-  }
+}
 
-	chat.inputSubmit = function(){
-		var message = $("#message_input").val();
-		if(message.length){
-			send.new_message(message);
-		}
-		$("#message_input").val("").focus();
-		return false;
-	}
-
-  chat.inputKeyPress = function(ev){
-    chat.user_activity();
-    if(ev.charCode == 13){ // Enter
-      if(!ev.shiftKey){ // Shift >> New Line
-        chat.stop_typing();
-        $("#input").submit();
-        ev.preventDefault();
-      }
-    }else{
-      chat.start_typing();
+chat.inputSubmit = function() {
+    var message = $("#message_input").val();
+    if (message.length) {
+        send.new_message(message);
     }
-  }
+    $("#message_input").val("").focus();
+    return false;
+}
 
-  // Receive events
+chat.inputKeyPress = function(ev) {
+    chat.user_activity();
+    if (ev.charCode == 13) {
+        // Enter
+        if (!ev.shiftKey) {
+            // Shift >> New Line
+            chat.stop_typing();
+            $("#input").submit();
+            ev.preventDefault();
+        }
+    } else {
+        chat.start_typing();
+    }
+}
 
-  receive.ready = function (data) {
-      if(chat.connected){
+// Receive events
+receive.ready = function(data) {
+    if (chat.connected) {
         chat.reconnecting = true;
-      }
-      send.auth();
-    };
+    }
+    send.auth();
+};
 
-  receive.auth_success = function(data){
-    if(chat.reconnecting){
-      chat.add_notice("You have been reconnected.");
-      chat.reconnecting = false;
-    }else{
-      chat.add_notice("Please wait while we connect you to a representative ...")
-		.addClass("connecting_notice");
+receive.auth_success = function(data) {
+    if (chat.reconnecting) {
+        chat.add_notice("You have been reconnected.");
+        chat.reconnecting = false;
+    } else {
+        chat.add_notice("Please wait while we connect you to a representative ...")
+        .addClass("connecting_notice");
     }
     chat.connected = true;
-    };
+};
 
-  receive.auth_error = function(error){
-    chat.add_notice("Error connecting to server: "+error, 'negative');
-    };
+receive.auth_error = function(error) {
+    chat.add_notice("Error connecting to server: " + error, 'negative');
+};
 
-  receive.call_connected = function(data){
-	chat.operator_name = data.name;
-	chat.add_notice("You are now chatting with <strong>"+chat.operator_name+"</strong>!", 'positive');
-	$(".connecting_notice").fadeOut(1000, function(){
-		$(this).remove();
-	});
-  };
+receive.call_connected = function(data) {
+    chat.operator_name = data.name;
+    chat.add_notice("You are now chatting with <strong>" + chat.operator_name + "</strong>!", 'positive');
+    $(".connecting_notice").fadeOut(1000,
+    function() {
+        $(this).remove();
+    });
+};
 
-  receive.self_message = function(data){
+receive.self_message = function(data) {
     chat.add_message("You", data.message, "self");
-  };
+};
 
-  receive.operator_message = function(data){
-	chat.add_message(chat.operator_name, data.message, "operator");
-  };
+receive.operator_message = function(data) {
+    chat.add_message(chat.operator_name, data.message, "operator");
+};
 
-  receive.messages_seen = function(data){
-	console.log("Messages seen by operator");
-  };
+receive.messages_seen = function(data) {
+    console.log("Messages seen by operator");
+};
 
-  receive.typing = function(data){
-	console.log("Operator has "+(data.typing?"started":"stopped")+" typing");
-  };
+receive.typing = function(data) {
+    console.log("Operator has " + (data.typing ? "started": "stopped") + " typing");
+};
 
-  // Send events
-	
-	send.auth = function(){
-		socket.emit('auth', auth);
-	};
+// Send events
+send.auth = function() {
+    socket.emit('auth', auth);
+};
 
-  send.read_message = function(id){
+send.read_message = function(id) {
     socket.emit("read_message", id);
-  };
+};
 
-  send.start_typing = function(){
-    socket.emit("typing", {typing: true});
-  };
+send.start_typing = function() {
+    socket.emit("typing", {
+        typing: true
+    });
+};
 
-  send.stop_typing = function(){
-    socket.emit("typing", {typing: false});
-  };
+send.stop_typing = function() {
+    socket.emit("typing", {
+        typing: false
+    });
+};
 
-  send.new_message = function(message){
-    socket.emit("new_message", { "message": message });
-  };
+send.new_message = function(message) {
+    socket.emit("new_message", {
+        "message": message
+    });
+};
 
-  function setup(){
+function setup() {
     socket = io.connect('http://localhost/chatter');
-    for(var i in receive){
-      socket.on(i, receive[i]);
+    for (var i in receive) {
+        socket.on(i, receive[i]);
     }
     $("#input").submit(chat.inputSubmit);
     $("#message_input")
-      .keypress(chat.inputKeyPress)
-      .focus(chat.user_activity)
-      .blur(chat.user_activity);
+    .keypress(chat.inputKeyPress)
+    .focus(chat.user_activity)
+    .blur(chat.user_activity);
     $(window).focus(chat.user_activity);
 
-	$("#login_form").submit(function(){
-		$.ajax({
-		  type: "POST",
-		  url: "/incoming_call",
-		  data: {
-			token: auth.token,
-			secret: auth.secret,
-			name: $("#login_name").val()||"Chatter",
-			operators: ["1","2","3","123"],
-			conversation_token: "hello_world"
-			},
-		  complete: function(data, status){
-			console.log(data, status)
-			send.auth();
-			},
-		  dataType: "application/json; charset=UTF-8"
-		});
-		return false;
-	});
-  }
+    $("#login_form").submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "/incoming_call",
+            data: {
+                token: auth.token,
+                secret: auth.secret,
+                name: $("#login_name").val() || "Chatter",
+                operators: ["1", "2", "3", "123"],
+                conversation_token: "hello_world"
+            },
+            complete: function(data, status) {
+                console.log(data, status)
+                send.auth();
+            },
+            dataType: "application/json; charset=UTF-8"
+        });
+        return false;
+    });
+}
 
-  $(document).ready(setup);
+$(document).ready(setup);
--- public/operator/chat.js
+++ public/operator/chat.js
@@ -1,240 +1,299 @@
 var auth = {
-      "secret": "456",
-      "token": "123"
-  },
-  socket = null,
-  receive = {},
-  send = {},
-	chat = {},
-	server = {};
+    "secret": "456",
+    "token": "123"
+},
+socket = null,
+receive = {},
+send = {},
+chat = {},
+server = {},
+originalTitle = "",
+title_interval = null,
+flash_delay = 300;
 
-	// Server
-	
-	server.connected = false;
-	server.reconnecting = false;
-	
-	// Chat
-	
-	chat.typing_timeout = null;
-	chat.unread_messages_here = false;
-	chat.unread_messages_there = false;
-	chat.chatting = false;
-	chat.conversation_token = null;
-
-	chat.add_notice = function(text, className){
-	  className = className || "";
-	  var notice = $('<div class="notice '+className+'">'+text+'</div>');
-	  $("#messages").append(notice);
-	  chat.scroll_messages();
-	  return notice;
-	}
-	chat.add_message = function(username, text, className){
-	  className = className || "";
-	  var message = $('<div class="message '+className+'"><span class="username">'+username+':</span>'+text+'</div>');
-	  $("#messages").append(message);
-	  chat.scroll_messages();
-	  return message;
-	}
-
-	chat.start_typing = function(){
-	  if(chat.typing_timeout){
-	    clearTimeout(chat.typing_timeout);
-		chat.typing_timeout = null;
-	  }else{
-	    send.start_typing();
-	  }
-	  chat.typing_timeout = setTimeout(chat.stop_typing, 1000);
-	  $("#message_input").css("border-color", "red");
-	}
-
-	chat.stop_typing = function(){
-	  clearTimeout(chat.typing_timeout);
-		chat.typing_timeout = null;
-	  send.stop_typing();
-	  $("#message_input").css("border-color", "#777777");
-	}
-
-	chat.user_activity = function(){
-	  if(chat.unread_messages_here){
-	    send.read_message();
-	    chat.unread_messages_here = false;
-	  }
-	  chat.scroll_messages();
-	}
-
-	chat.scroll_messages = function(){
-	  $("#scroll").scrollTop($("#scroll")[0].scrollHeight);
-	}
-	
-	chat.openChat = function(config){
-		chat.build();
-		chat.chatting = true;
-		// TODO: Load previous conversation messages
-		chat.add_notice("You are now chatting with <strong>"+config.name+"</strong>!", "positive");
-		chat.conversation_token = config.conversation_token;
-		chat.chatter_name = config.name;
-		chat.chatter_token = config.chatter_token;
-	}
-	
-	chat.closeChat = function(){
-		chat.chatting = false;
-	}
-	
-	chat.build = function(){
-		var content = $("#content")
-			.html('<div id="display"></div>')
-			.append(
-				$('<div id="chat"></div>')
-					.append(
-						$('<div id="scroll"></div>')
-							.append($('<div id="messages"></div>'))
-					)
-					.append(
-						$('<form id="input"></form>')
-							.append(
-								$('<textarea id="message_input"></textarea>')
-									.keypress(chat.inputKeyPress)
-									.focus(chat.user_activity)
-								    .blur(chat.user_activity)
-							)
-							.append($('<input id="send" type="submit" value="Send">'))
-							.submit(chat.inputSubmit)
-					)
-			);
-	}
-	
-	chat.inputSubmit = function(){
-	    var message = $("#message_input").val();
-	    if(message.length){
-	      send.new_message(message);
-	    }
-	    $("#message_input").val("").focus();
-	    return false;
-	  }
-
-	chat.inputKeyPress = function(ev){
-	    chat.user_activity();
-	    if(ev.charCode == 13){ // Enter
-	      if(!ev.shiftKey){ // Shift >> New Line
-	        chat.stop_typing();
-	        $("#input").submit();
-	        ev.preventDefault();
-	      }
-	    }else{
-	      chat.start_typing();
-	    }
-	  };
-	
-	chat.window_focus = function(){
-		if(chat.chatting){
-			chat.user_activity();
+function flashTitle(msg){
+	if(!title_interval){
+		originalTitle = document.title;
+		var blink = function(){
+			document.title = document.title == msg ? '!!!':msg;
+			title_interval = setTimeout(blink, flash_delay);
 		}
+		blink();
 	}
+}
 
-  // Receive events
+function resetTitle(title){
+	if(title_interval){
+		clearTimeout(title_interval);
+		title_interval = null;
+	}
+	document.title = title ? title : originalTitle;
+}
 
-  receive.ready = function (data) {
-      send.auth();
-    };
+// Server
+server.connected = false;
+server.reconnecting = false;
 
-  receive.auth_success = function(data){
+// Chat
+chat.typing_timeout = null;
+chat.unread_messages_here = false;
+chat.unread_messages_there = false;
+chat.chatting = false;
+chat.conversation_token = null;
+
+chat.add_notice = function(text, className) {
+    className = className || "";
+    var notice = $('<div class="notice ' + className + '">' + text + '</div>');
+    $("#messages").append(notice);
+    chat.scroll_messages();
+    return notice;
+}
+chat.add_message = function(username, text, className) {
+    className = className || "";
+    var message = $('<div class="message ' + className + '"><span class="username">' + username + ':</span>' + text + '</div>');
+    $("#messages").append(message);
+    chat.scroll_messages();
+    return message;
+}
+
+chat.start_typing = function() {
+    if (chat.typing_timeout) {
+        clearTimeout(chat.typing_timeout);
+        chat.typing_timeout = null;
+    } else {
+        send.start_typing();
+    }
+    chat.typing_timeout = setTimeout(chat.stop_typing, 1000);
+    $("#message_input").css("border-color", "red");
+}
+
+chat.stop_typing = function() {
+    clearTimeout(chat.typing_timeout);
+    chat.typing_timeout = null;
+    send.stop_typing();
+    $("#message_input").css("border-color", "#777777");
+}
+
+chat.user_activity = function() {
+	if(title_interval){
+		resetTitle();
+	}
+    if (chat.unread_messages_here) {
+        send.read_message();
+        chat.unread_messages_here = false;
+    }
+    chat.scroll_messages();
+}
+
+chat.scroll_messages = function() {
+    $("#scroll").scrollTop($("#scroll")[0].scrollHeight);
+}
+
+chat.openChat = function(config) {
+    chat.build();
+    chat.chatting = true;
+    // TODO: Load previous conversation messages
+    chat.add_notice("You are now chatting with <strong>" + config.name + "</strong>!", "positive");
+    chat.conversation_token = config.conversation_token;
+    chat.chatter_name = config.name;
+    chat.chatter_token = config.chatter_token;
+}
+
+chat.closeChat = function() {
+    chat.chatting = false;
+}
+
+chat.typing_notice_before = "";
+chat.typing_notice_timer = null;
+chat.show_typing_notice = function(){
+	chat.clear_typing_notice(true);
+	chat.typing_notice_before = $("#typing")[0].innerHTML;
+	$("#typing").html(chat.chatter_name+" is typing ...").show();
+	//chat.typing_notice_timer = setTimeout(chat.hide_typing_notice, 2000);
+}
+
+chat.hide_typing_notice = function(){
+	chat.clear_typing_notice();
+	$("#typing").fadeOut(400, function(){
+		$(this).html(chat.typing_notice_before||"").show();
+	});
+}
+
+chat.clear_typing_notice = function(reset){
+	if(chat.typing_notice_timer){
+		clearTimeout(chat.typing_notice_timer);
+		chat.typing_notice_timer = null;
+		if(reset){ $("#typing").html(chat.typing_notice_before||"").show(); }
+	}
+}
+
+chat.build = function() {
+    var content = $("#content")
+    .html('<div id="display"></div>')
+    .append(
+    $('<div id="chat"></div>')
+    .append(
+    $('<div id="scroll"></div>')
+    .append($('<div id="messages"></div>'))
+    )
+	.append('<div id="typing"></div>')
+    .append(
+    $('<form id="input"></form>')
+    .append(
+    $('<textarea id="message_input"></textarea>')
+    .keypress(chat.inputKeyPress)
+    .focus(chat.user_activity)
+    .blur(chat.user_activity)
+    )
+    .append($('<input id="send" type="submit" value="Send">'))
+    .submit(chat.inputSubmit)
+    )
+    );
+}
+
+chat.inputSubmit = function() {
+    var message = $("#message_input").val();
+    if (message.length) {
+        send.new_message(message);
+    }
+    $("#message_input").val("").focus();
+    return false;
+}
+
+chat.inputKeyPress = function(ev) {
+    chat.user_activity();
+    if (ev.charCode == 13) {
+        // Enter
+        if (!ev.shiftKey) {
+            // Shift >> New Line
+            chat.stop_typing();
+            $("#input").submit();
+            ev.preventDefault();
+        }
+    } else {
+        chat.start_typing();
+    }
+};
+
+chat.window_focus = function() {
+    if (chat.chatting) {
+        chat.user_activity();
+    }
+}
+
+// Receive events
+receive.ready = function(data) {
+    send.auth();
+};
+
+receive.auth_success = function(data) {
     //alert("Connected to server");
     };
 
-  receive.auth_error = function(error){
-    alert("Error connecting to server: "+error);
-  };
+receive.auth_error = function(error) {
+    alert("Error connecting to server: " + error);
+};
 
-  receive.call_request = function(data){
-	var token = data.conversation_token,
-		dialog = $("#call_dialog"),
-		profile = $('<div class="call_profile"></div>'),
-		accept = $('<input type="button" value="Accept" id="accept_call">')
-			.click(function(){
-				send.accept_call(token);
-				$("#modal").hide();
-			}),
-		decline = $('<input type="button" value="Decline" id="decline_call">')
-			.click(function(){
-				send.decline_call(token);
-				$("#modal").hide();
-			});
-	
-	dialog.html("<h2>Incoming Call</h2>");
-	dialog.append(profile);
-	dialog.append($('<div class="call_buttons"></div>').append(accept).append(decline));
-	
-	$("#modal").show();
-  };
+receive.call_request = function(data) {
+    var token = data.conversation_token,
+    dialog = $("#call_dialog"),
+    profile = $('<div class="call_profile"></div>'),
+    accept = $('<input type="button" value="Accept" id="accept_call">')
+    .click(function() {
+		resetTitle();
+        send.accept_call(token);
+        $("#modal").hide();
+    }),
+    decline = $('<input type="button" value="Decline" id="decline_call">')
+    .click(function() {
+		resetTitle();
+        send.decline_call(token);
+        $("#modal").hide();
+    });
 
-  receive.call_connected = function(data){
-	chat.openChat(data);
-  };
+    dialog.html("<h2>Incoming Call</h2>");
+    dialog.append(profile);
+    dialog.append($('<div class="call_buttons"></div>').append(accept).append(decline));
 
-  receive.call_declined = function(data){
-	alert("The call from "+data.name+" has already been accepted by somebody else.")
-  };
+    $("#modal").show();
+	flashTitle("Incoming Call ...");
+};
 
-  receive.self_message = function(data){
+receive.call_connected = function(data) {
+    chat.openChat(data);
+};
+
+receive.call_declined = function(data) {
+    alert("The call from " + data.name + " has already been accepted by somebody else.")
+};
+
+receive.self_message = function(data) {
     chat.add_message("You", data.message, "self");
-  };
+};
 
-  receive.chatter_message = function(data){
-	chat.add_message(chat.chatter_name, data.message, "other");
-  };
+receive.chatter_message = function(data) {
+    chat.add_message(chat.chatter_name, data.message, "other");
+};
 
-  receive.messages_seen = function(data){
-	console.log("Messages seen by chatter");
-  };
+receive.messages_seen = function(data) {
+    console.log("Messages seen by chatter");
+};
 
-  receive.typing = function(data){
-	console.log("Chatter has "+(data.typing?"started":"stopped")+" typing");
-  };
+receive.typing = function(data) {
+	if(data.typing){
+		chat.show_typing_notice();
+	}else{
+		chat.hide_typing_notice();
+	}
+    console.log("Chatter has " + (data.typing ? "started": "stopped") + " typing");
+};
 
-  // Send events
+// Send events
+send.auth = function() {
+    socket.emit('auth', auth);
+};
 
-	send.auth = function(){
-		socket.emit('auth', auth);
-	};
-
-  send.accept_call = function(token){
+send.accept_call = function(token) {
     socket.emit("accept_call", token);
-  };
+};
 
-  send.decline_call = function(token){
+send.decline_call = function(token) {
     socket.emit("decline_call", token);
-  };
+};
 
-  send.read_message = function(){
+send.read_message = function() {
     socket.emit("read_message", {
-		conversation_token: chat.conversation_token
-	});
-  };
+        conversation_token: chat.conversation_token
+    });
+};
 
-  send.start_typing = function(){
+send.start_typing = function() {
     socket.emit("typing", {
-		conversation_token: chat.conversation_token,
-		typing: true
-	});
-  };
+        conversation_token: chat.conversation_token,
+        typing: true
+    });
+};
 
-  send.stop_typing = function(){
+send.stop_typing = function() {
     socket.emit("typing", {
-		conversation_token: chat.conversation_token,
-		typing: false
-	});
-  };
+        conversation_token: chat.conversation_token,
+        typing: false
+    });
+};
 
-  send.new_message = function(message){
-    socket.emit("new_message", { "message": message, conversation_token: chat.conversation_token});
-  };
+send.new_message = function(message) {
+    socket.emit("new_message", {
+        "message": message,
+        conversation_token: chat.conversation_token
+    });
+};
 
-  function setup(){
+function setup() {
     socket = io.connect('http://localhost/operator');
-    for(var i in receive){
-      socket.on(i, receive[i]);
+    for (var i in receive) {
+        socket.on(i, receive[i]);
     }
-	$(window).focus(chat.window_focus);
-  }
+    $(window).focus(chat.window_focus);
+}
 
-  $(document).ready(setup);
+$(document).ready(setup);
